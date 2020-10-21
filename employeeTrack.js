d33e021dea17b93704f9ba0c6d7aba6b9c5a5d86
Brian Lee <brianlee@Brians-MBP.hsd1.ca.comcast.net>
2020-10-20 20:03:10 -0700

    all prompt functions included

--- employeeTrack.js
+++ employeeTrack.js
@@ -3,45 +3,88 @@ var inquirer = require("inquirer");
 var consoleTable = require("console.table");
 var conection = mysql.createConnection({
     host: "localhost",
-  
+
     // Your port; if not 3306
     port: 3306,
-  
+
     // Your username
     user: "root",
-  
+
     // Your password
     password: "password",
     database: "top_songsDB"
-  });
+});
 
-  connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     editTables();
-  });
+});
 
-  function editTables(){
-  inquirer
-    .prompt({
-      name: "action",
-      type: "rawlist",
-      message: "What would you like to do?",
-      choices: [
-        "Add Departments, Roles, or Employees",
-        "View Departments, Roles, or Employees",
-        "Find data within a specific range",
-        "Update Employee Roles"
-      ],
-      name: "add",
-      type: "list",
-      message: "What would you like to add?"
-      choices: [
-        "Department",
-        "Roles",
-        "Employees"
-      ]
+function editTables() {
+    inquirer
+        .prompt({
+            name: "action",
+            type: "rawlist",
+            message: "What would you like to do?",
+            choices: [
+                "Add Departments, Roles, or Employees",
+                "View Departments, Roles, or Employees",
+                "Update Employee Roles"
+            ]
+        }).then(function (answer) {
+            switch (answer.action) {
+                case "Add Departments, Roles, or Employees":
+                    promptAdd();
+                    break;
+                case "View Departments, Roles, or Employees":
+                    promptView();
+                    break;
+                case "Update Employee Roles":
+                    promptUpdate();
+                    break;
+            }
+        })
 
-    })
+}
 
+function promptAdd() {
+    inquirer
+        .prompt({
+            name: "add",
+            type: "rawlist",
+            message: "What would you like to do?",
+            choices: [
+                "Department",
+                "Roles",
+                "Employees"
+            ]
+        })
+}
 
+function promptView() {
+    inquirer
+        .prompt({
+            name: "view",
+            type: "rawlist",
+            message: "What would you like to do?",
+            choices: [
+                "Department",
+                "Roles",
+                "Employees"
+            ]
+        })
+}
+
+function promptUpdate() {
+    inquirer
+        .prompt({
+            name: "update",
+            type: "rawlist",
+            message: "What would you like to do?",
+            choices: [
+                "Department",
+                "Roles",
+                "Employees"
+            ]
+        })
 }
